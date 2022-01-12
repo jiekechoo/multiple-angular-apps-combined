791423502531bc4d29076070b734a48f16e56304
Jieke Choo <jiekechoo@gmail.com>
2022-01-12 18:30:03 +0800

    first commit

---
+++ projects/app1/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { ModuleWithProviders } from '@angular/compiler/src/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { View1Component } from './view1/view1.component';
+import { View2Component } from './view2/view2.component';
+
+const providers: any = [];
+
+@NgModule({
+  declarations: [AppComponent, View1Component, View2Component, NavComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers,
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+@NgModule({})
+export class App1SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: AppModule,
+      providers,
+    };
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { App1SharedModule } from 'projects/app1/src/app/app.module';
+import { App2SharedModule } from 'projects/app2/src/app/app.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    App1SharedModule.forRoot(),
+    App2SharedModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
